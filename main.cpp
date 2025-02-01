b098714b7504d5dd6c1bb8340f9ffb289982eebe
vaanuthanesh2003 <vaanathythaneskumar@cmail.carleton.ca>
2025-02-01 14:26:14 -0500

    Update main.cpp
    feat: adding request class structure

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
 
+class Request {
+private:
+    int time;
+    int floor;
+    std::string button;
+
+public:
+    Request(int t, int f, std::string b) : time(t), floor(f), button(b) {}
+
+    std::string get_button() { return button; }
+    int get_floor() { return floor; }
+    int get_time() { return time; }
+
+    void set_button(std::string b) { button = b; }
+    void set_floor(int f) { floor = f; }
+    void set_time(int t) { time = t; }
+
+    void Tostring() {
+        std::cout << "Button: " << button << std::endl;
+        std::cout << "Floor: " << floor << std::endl;
+        std::cout << "Time: " << time << std::endl;
+    }
+};
