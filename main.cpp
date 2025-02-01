7d59d238a4399ced63cc56f2859551290fb5b183
vaanuthanesh2003 <vaanathythaneskumar@cmail.carleton.ca>
2025-02-01 14:27:11 -0500

    Update main.cpp
    adding Elevator class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <string>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <conditional_variable>
+#include <vector>
 
 class Request {
 private:
@@ -24,3 +29,28 @@ public:
         std::cout << "Time: " << time << std::endl;
     }
 };
+
+// initializing global variables
+
+std::queue<Request> requestQueue;
+std:: mutex queueMutex;
+std:: conditional_variable cond;
+
+void Elevator(){
+    
+    while (true){
+        // in this class, the "processing" of a request is what will simulate the car moving
+        std::unique_lock<std::mutex> lock(queueMutex); // receive lock to access shared queue
+        cond.wait(lock, [] { return !requestQueue.empty(); }); // wait for a request in the queue
+
+        Request req = requestQueue.front(); // this allows us to get the first request from the queue (FIFO)
+        requestQueue.pop(); // removes top request from queue as it is being handled
+        lock.unlock(); // release lock to allow other threads to access it
+
+        std::cout << "[Elevator] Moving to floor " << req.source << " to handle request.\n";
+        std::this_thread::sleep_for(std::chrono::seconds(3)); // this will simulate "moving" to requested floor
+
+        std::cout << "[Elevator] Request from floor " << req.source << " handled.\n" // output that request is handled
+    }
+
+}
